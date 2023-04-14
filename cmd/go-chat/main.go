f9f30acd75f6b444a4431f54470fbcb552692824
Lawrence Yolland <lawrence.yolland@ericsson.com>
2023-04-14 19:30:12 +0100

    landing message

--- cmd/go-chat/main.go
+++ cmd/go-chat/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	// "log"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = "8000"
+
+func tempHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	message := fmt.Sprintf("<h1>Hi! Welcome to %s</h1>", path)
+	w.Write([]byte(message))
+}
 
 func main() {
-	fmt.Print("Hi")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", tempHandler)
+
+	fmt.Printf("Listening on %s...", PORT)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), mux)
+
+	if err != nil {
+		log.Fatalln("Server Error:", err)
+	}
 }
